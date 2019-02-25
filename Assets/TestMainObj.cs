a566c6bb3e5bd9795057d9a60f271477b69d7afb
uruza <any2dev@gmail.com>
2019-02-25 14:43:28 +0900

    test

---
+++ Assets/TestMainObj.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class TestMainObj : MonoBehaviour
+{
+    private void Awake()
+    {
+        CreateTestObj( "AAA" );
+        CreateTestObj( "BBB" );
+        CreateTestObj( "CCC" );
+    }
+
+    void CreateTestObj ( string objName )
+    {
+        GameObject go = new GameObject( objName );
+        go.AddComponent<TestCallOrder>();
+        go.transform.SetParent( transform, false );
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
